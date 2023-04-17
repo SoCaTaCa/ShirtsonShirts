a103a7b2183c72d05121ee17afe6bcdd85c95edd
Soren Axelson <sorenaxelson@gmail.com>
2023-04-17 10:18:19 -0500

    fixed db users functions

--- db/users.js
+++ db/users.js
@@ -1,58 +1,54 @@
 // inserts data into users table
 const createUser = async ({ username, password, isAdmin }) => {
   try {
-    const user = await client.query(
+    const {
+      roes: [user],
+    } = await client.query(
       `
     INSERT INTO users(username, password, isAdmin)
-    VALUES ($1, $2, $3)
+    VALUES (${username}, ${password}, ${isAdmin})
     RETURNING id, username, password, isAdmin;
-    `,
-      [username, password, isAdmin]
+    `
     );
     return user;
   } catch {
-    console.log("createUser error");
+    console.log("createUser error", err);
   }
 };
-// createUser test
-console.log(createUser("jimmothy", "password", false));
 
 // takes in a user ID and returns the corresponding user
 const getUserById = async (userId) => {
   try {
-    const user = await client.query(
+    const {
+      rows: [user],
+    } = await client.query(
       `
       SELECT *
       FROM users
-      WHERE id=$1
+      WHERE id= ${userId}
     `,
       [userId]
     );
     return user;
   } catch (err) {
-    console.log("getUserByID error");
+    console.log("getUserByID error", err);
   }
 };
-// getUserById test
-// will only work if createUser is functional
-console.log(getUserById(createUser("jimothy", "password", false).id));
 
 // takes in a username and returns the corresponding user
 const getUserByUsername = async (username) => {
   try {
-    const user = await client.query(
+    const {
+      rows: [user],
+    } = await client.query(
       `
       SELECT *
       FROM users
-      WHERE username=$1
-    `,
-      [username]
+      WHERE username=${username}
+    `
     );
     return user;
   } catch (err) {
-    console.log("getUserByUsername error");
+    console.log("getUserByUsername error", err);
   }
 };
-// getUserByUsername test
-// will only work if createUser is functional
-console.log(getUserByUsername(createUser("jimothy", "password", false).id));

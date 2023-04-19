a886ecf5efc41077e74cec5d6d5bbbde18be299a
Tate Redding <tateredding@gmail.com>
2023-04-19 14:39:22 -0600

    added a post route to /api/categories

--- api/categories.js
+++ api/categories.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getAllCategories } = require('../db/categories');
+const { getAllCategories, createCategory } = require('../db/categories');
 
 router.get('/', async (req, res) => {
     try {
         const categories = await getAllCategories();
         res.send(categories);
+    } catch (error) {
+        console.error(error);
+    };
+});
+
+router.post('/', async (req, res) => {
+    const { name } = req.body;
+    try {
+        const category = await createCategory(name);
+        res.send(category);
     } catch (error) {
         console.error(error);
     };

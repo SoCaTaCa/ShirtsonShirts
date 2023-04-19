55cdb2fa5f715ed96f00f78f878aa2ab8838368e
Tate Redding <tateredding@gmail.com>
2023-04-19 14:31:27 -0600

    added get route to /api/categories #51

--- api/categories.js
+++ api/categories.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { getAllCategories } = require('../db/categories');
+
+router.get('/', async (req, res) => {
+    try {
+        const categories = await getAllCategories();
+        res.send(categories);
+    } catch (error) {
+        console.error(error);
+    };
+});
 
 module.exports = router;

5d0c01e9672b47d9d840760e69065f5a07be2fa0
Tate Redding <tateredding@gmail.com>
2023-04-19 12:30:55 -0600

    created a route to /api/users/login #41

--- api/users.js
+++ api/users.js
@@ -1,4 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+const { getUser } = require('../db/users');
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        if (!username || !password) {
+            return;
+        };
+        const user = await getUser({ username, password })
+        if (user) {
+            const token = jwt.sign({
+                id: user.id,
+                username
+            }, process.env.JWT_SECRET, {
+                expiresIn: '1w'
+            });
+            res.send({
+                success: true,
+                message: 'Login succesful!',
+                token,
+                user
+            });
+        } else {
+            res.send({
+                success: false,
+                error: 'IncorrectCredentialsError',
+                message: 'Username and password do not match!'
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    };
+});
 
 module.exports = router;

59fe0c354d755e8511d2baf17721dc0bf425dbf5
Cass <cass@Casss-MBP.attlocal.net>
2023-04-20 10:16:46 -0500

    created the register react component #55

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.js';
 import Header from './components/Header.js';
 import Login from './components/Login.js';
+import Register from './components/Register.js';
 
 const App = () => {
     const [userToken, setUserToken] = useState('');
@@ -15,6 +16,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/login' element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUserToken={setUserToken}/>}></Route>
+                <Route path='/register' element={<Register setUserToken={setUserToken} setIsLoggedIn={setIsLoggedIn}/>}></Route>
             </Routes>
         </>
     )

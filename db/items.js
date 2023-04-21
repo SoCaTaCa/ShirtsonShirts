6b9cd8ec8b1cc876262d5b996e5db098c4565512
Pruna <CaroPruna@pm.me>
2023-04-21 01:37:53 -0400

    Made a getItemsByName function in db/items #22

--- db/items.js
+++ db/items.js
@@ -1,5 +1,6 @@
-const client = require('./db/client');
+const client = require('./client');
 
+//createItem function
 const createItem = async ({ name, price, size, categoryId, description }) => {
   const query = {
     text: 'INSERT INTO items(name, price, size, category_id, description) VALUES($1, $2, $3, $4, $5) RETURNING *',
@@ -10,4 +11,18 @@ const createItem = async ({ name, price, size, categoryId, description }) => {
   return rows[0];
 };
 
-  
+//getItemsByName function
+const getItemsByName = async (name) => {
+  const query = {
+    text: 'SELECT * FROM items WHERE name ILIKE $1',
+    values: [`%${name}%`],
+  };
+
+  const { rows } = await client.query(query);
+  return rows;
+};
+
+module.exports = {
+  createItem,
+  getItemsByName,
+};

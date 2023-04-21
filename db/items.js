01f63be34b225e4107bc7aa44ffeb5d27a166e77
Pruna <CaroPruna@pm.me>
2023-04-21 01:41:10 -0400

    Made a getItemsByName function in db/items #22

--- db/items.js
+++ db/items.js
@@ -1,6 +1,5 @@
 const client = require('./client');
 
-//createItem function
 const createItem = async ({ name, price, size, categoryId, description }) => {
   const query = {
     text: 'INSERT INTO items(name, price, size, category_id, description) VALUES($1, $2, $3, $4, $5) RETURNING *',
@@ -11,7 +10,35 @@ const createItem = async ({ name, price, size, categoryId, description }) => {
   return rows[0];
 };
 
-//getItemsByName function
+const getAllItems = async () => {
+  const query = {
+    text: 'SELECT * FROM items',
+  };
+
+  const { rows } = await client.query(query);
+  return rows;
+};
+
+const getItemById = async (id) => {
+  const query = {
+    text: 'SELECT * FROM items WHERE id = $1',
+    values: [id],
+  };
+
+  const { rows } = await client.query(query);
+  return rows[0];
+};
+
+const getItemsByCategory = async (categoryId) => {
+  const query = {
+    text: 'SELECT * FROM items WHERE category_id = $1',
+    values: [categoryId],
+  };
+
+  const { rows } = await client.query(query);
+  return rows;
+};
+
 const getItemsByName = async (name) => {
   const query = {
     text: 'SELECT * FROM items WHERE name ILIKE $1',
@@ -24,5 +51,8 @@ const getItemsByName = async (name) => {
 
 module.exports = {
   createItem,
+  getAllItems,
+  getItemById,
+  getItemsByCategory,
   getItemsByName,
 };

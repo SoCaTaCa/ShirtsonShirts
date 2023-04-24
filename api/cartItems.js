da1aad8e13c2c92e26339f15274cf578d717b0d1
Tate Redding <tateredding@gmail.com>
2023-04-24 15:01:01 -0600

    added a patch route to /api/cartItems/:cartItemId

--- api/cartItems.js
+++ api/cartItems.js
@@ -1,4 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const { requireUser } = require('./utils');
+const { getCartItemById, updateCartItem } = require("../db/cartItems");
+const { getCartById } = require('../db/carts');
+
+router.patch('/:cartItemId', requireUser, async (req, res) => {
+    const { cartItemId } = req.params;
+    const { quantity } = req.body;
+    try {
+        const _cartItem = await getCartItemById(cartItemId);
+        if (_cartItem) {
+            const cart = await getCartById(_cartItem.cartId);
+            if (cart.userId === req.user.id) {
+                if (cart.isPurchased) {
+                    res.send({
+                        success: false,
+                        error: 'CartAlreadyPurchased',
+                        message: 'You can not update an item from a previous order!'
+                    });
+                } else {
+                    const cartItem = await updateCartItem(cartItemId, quantity);
+                    if (cartItem) {
+                        res.send({
+                            success: true,
+                            cartItem
+                        });
+                    }
+                }
+            } else {
+                res.send({
+                    success: false,
+                    error: 'UnauthorizedUserError',
+                    message: 'You can not modify an item in a cart that is not yours!'
+                });
+            };
+        } else {
+            res.send({
+                success: false,
+                error: 'InvalidCartItemId',
+                message: `Can not find a cart item with id ${cartItemId}`
+            })
+        }
+
+    } catch (error) {
+        console.error(error);
+    };
+
+})
+
 
 module.exports = router;
--- tests/api/cartItems.js
+++ tests/api/cartItems.js
@@ -7,6 +7,59 @@ const { rebuildDB, seedDB } = require('../../db/seedData');
 const test = async () => {
     console.log('--- RUNNING api/cartItems TESTS ---');
     try {
+        let loginResponse = await request(app)
+            .post('/api/users/login')
+            .send({
+                username: 'cdoussan',
+                password: 'apples45'
+            });
+        const tokenOne = loginResponse.body.token;
+
+        loginResponse = await request(app)
+            .post('/api/users/login')
+            .send({
+                username: 'tredding',
+                password: 'slushies89'
+            });
+        const tokenTwo = loginResponse.body.token;
+
+        console.log('Testing patch to /:cartItemId');
+        const goodResponse = await request(app)
+            .patch('/api/cartItems/3')
+            .set('Authorization', `Bearer ${tokenOne}`)
+            .send({
+                quantity: 100
+            });
+
+        const badResponseOne = await request(app)
+            .patch('/api/cartItems/2')
+            .set('Authorization', `Bearer ${tokenOne}`)
+            .send({
+                quantity: 100
+            });
+
+        const badResponseTwo = await request(app)
+            .patch('/api/cartItems/2')
+            .set('Authorization', `Bearer ${tokenTwo}`)
+            .send({
+                quantity: 100
+            });
+
+        const badResponseThree = await request(app)
+            .patch('/api/cartItems/200')
+            .set('Authorization', `Bearer ${tokenTwo}`)
+            .send({
+                quantity: 100
+            });
+
+        if (goodResponse.body.cartItem.quantity === 100 &&
+            badResponseOne.body.error === 'UnauthorizedUserError' &&
+            badResponseTwo.body.error === 'CartAlreadyPurchased' &&
+            badResponseThree.body.error === 'InvalidCartItemId') {
+            console.log('passed');
+        } else {
+            console.log('FAILED');
+        };
 
     } catch (err) {
         console.log('Error runnning tests!', err);

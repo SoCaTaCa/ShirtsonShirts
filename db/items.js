673d1c2eedfb05fd67dc5d0b2cb5f42bc9c818dc
Pruna <CaroPruna@pm.me>
2023-04-26 02:18:20 -0400

    Made updateItem function in db/items #17

--- db/items.js
+++ db/items.js
@@ -49,10 +49,33 @@ const getItemsByName = async (name) => {
   return rows;
 };
 
+const updateItem = async ({ id, name, price, size, categoryId, description }) => {
+  const query = {
+    text: 'UPDATE items SET name = $2, price = $3, size = $4, category_id = $5, description = $6 WHERE id = $1 RETURNING *',
+    values: [id, name, price, size, categoryId, description]
+  };
+
+  const { rows } = await client.query(query);
+  return rows[0];
+};
+
+const destroyItem = async (id) => {
+  await client.query('DELETE FROM user_items WHERE item_id = $1', [id]);
+  const query = {
+    text: 'DELETE FROM items WHERE id = $1 RETURNING *',
+    values: [id],
+  };
+
+  const { rows } = await client.query(query);
+  return rows[0];
+};
+
 module.exports = {
   createItem,
   getAllItems,
   getItemById,
   getItemsByCategory,
   getItemsByName,
+  updateItem,
+  destroyItem,
 };

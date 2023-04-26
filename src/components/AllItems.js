e7dc1a3b6563fd756016663806f34d0c3b625ca7
Cass <cass@Casss-MBP.attlocal.net>
2023-04-26 10:23:38 -0500

    Have all products display and mapped in AllItems React component #141

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const AllItems = () => {
+    const [items, setItems] = useState([]);
+
+    const getItems = async () => {
+        try {
+            const response = await axios.get('/api/items');
+            setItems(response.data.items);
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        getItems();
+    }, [])
+
     return (
-        <h1>Items displayed here.</h1>
+        <>
+            <ul>
+                {
+                    items.map((item, index) => {
+                        return <li key={index}>{item.name}</li>
+                    })
+                }
+            </ul>
+        </>
     )
 }
 

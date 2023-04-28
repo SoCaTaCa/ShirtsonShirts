cc0ddfecfb6019c4f4e5137822f2d389332b8271
Tate Redding <tateredding@gmail.com>
2023-04-28 15:14:47 -0600

    created form for admins to create a new category #190

---
+++ src/components/NewCategoryForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const NewCategoryForm = ({ userToken }) => {
+    const [name, setName] = useState('');
+    const [categoryExistsError, setCategoryExistsError] = useState(false);
+
+    const addCategory = async (event) => {
+        event.preventDefault();
+        setCategoryExistsError(false);
+
+        try {
+            const _category = await axios.get(`/api/categories/${name}`);
+            if (_category.data.success) {
+                setCategoryExistsError(true);
+            } else {
+                const category = await axios.post('/api/categories', { name }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${userToken}`
+                    }
+                });
+                if (category.data.success) {
+                    setName('');
+                };
+            };
+        } catch (error) {
+            console.error(error);
+        };
+    };
+
+    return (
+        <form onSubmit={addCategory} className="mb-3">
+            <label htmlFor="category-name" className="form-label">Name *</label>
+            <input
+                className="form-control"
+                id="category-name"
+                value={name}
+                required
+                onChange={(event) => setName(event.target.value)}>
+            </input>
+            {
+                categoryExistsError ?
+                    <div id="category-exists-text" className="form-text">
+                        That category already exists!
+                    </div> :
+                    null
+            }
+            <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={
+                    name ?
+                        false :
+                        true
+                }>
+                Add Category</button>
+        </form>
+    );
+};
+
+export default NewCategoryForm;
